637cc7fed82e04adb3f383a3473f65fc2846a84f
denial <danpan1232@gmail.com>
2024-01-27 12:49:54 -0500

    Update window GUI

--- window.Designer.cs
+++ window.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button_browse_files = new Button();
             SuspendLayout();
+            // 
+            // button_browse_files
+            // 
+            button_browse_files.Location = new Point(12, 12);
+            button_browse_files.Name = "button_browse_files";
+            button_browse_files.Size = new Size(75, 23);
+            button_browse_files.TabIndex = 0;
+            button_browse_files.Text = "Browse ...";
+            button_browse_files.UseVisualStyleBackColor = true;
+            button_browse_files.Click += button_browse_files_click;
             // 
             // danverter
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button_browse_files);
             Name = "danverter";
             Text = "danverter";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button_browse_files;
     }
 }
