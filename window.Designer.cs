8abd67219a25cdf7358b38c01925719138c7746f
denial <danpan1232@gmail.com>
2024-01-27 18:21:27 -0500

    Update GUI

--- window.Designer.cs
+++ window.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace danverter
 {
-    partial class danverter
+    partial class Danverter
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,6 +30,9 @@
         {
             button_browse_files = new Button();
             groupbox_select_files = new GroupBox();
+            button_clear = new Button();
+            button_delete_selected_files = new Button();
+            display_selected_files = new CheckedListBox();
             label_description = new Label();
             groupbox_select_files.SuspendLayout();
             SuspendLayout();
@@ -47,6 +50,9 @@
             // groupbox_select_files
             // 
             groupbox_select_files.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupbox_select_files.Controls.Add(button_clear);
+            groupbox_select_files.Controls.Add(button_delete_selected_files);
+            groupbox_select_files.Controls.Add(display_selected_files);
             groupbox_select_files.Controls.Add(label_description);
             groupbox_select_files.Controls.Add(button_browse_files);
             groupbox_select_files.Location = new Point(12, 12);
@@ -55,6 +61,37 @@
             groupbox_select_files.TabIndex = 1;
             groupbox_select_files.TabStop = false;
             groupbox_select_files.Text = "Select Files";
+            // 
+            // button_clear
+            // 
+            button_clear.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button_clear.Location = new Point(695, 223);
+            button_clear.Name = "button_clear";
+            button_clear.Size = new Size(75, 23);
+            button_clear.TabIndex = 5;
+            button_clear.Text = "Clear";
+            button_clear.UseVisualStyleBackColor = true;
+            button_clear.Click += button_clear_click;
+            // 
+            // button_delete_selected_files
+            // 
+            button_delete_selected_files.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button_delete_selected_files.Location = new Point(571, 223);
+            button_delete_selected_files.Name = "button_delete_selected_files";
+            button_delete_selected_files.Size = new Size(118, 23);
+            button_delete_selected_files.TabIndex = 4;
+            button_delete_selected_files.Text = "Delete selected";
+            button_delete_selected_files.UseVisualStyleBackColor = true;
+            button_delete_selected_files.Click += button_delete_selected_files_click;
+            // 
+            // display_selected_files
+            // 
+            display_selected_files.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            display_selected_files.FormattingEnabled = true;
+            display_selected_files.Location = new Point(6, 51);
+            display_selected_files.Name = "display_selected_files";
+            display_selected_files.Size = new Size(764, 166);
+            display_selected_files.TabIndex = 3;
             // 
             // label_description
             // 
@@ -65,13 +102,13 @@
             label_description.TabIndex = 2;
             label_description.Text = "You can select multiple files!";
             // 
-            // danverter
+            // Danverter
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(groupbox_select_files);
-            Name = "danverter";
+            Name = "Danverter";
             Text = "danverter";
             groupbox_select_files.ResumeLayout(false);
             groupbox_select_files.PerformLayout();
@@ -83,5 +120,8 @@
         private Button button_browse_files;
         private GroupBox groupbox_select_files;
         private Label label_description;
+        private CheckedListBox display_selected_files;
+        private Button button_delete_selected_files;
+        private Button button_clear;
     }
 }
--- window.cs
+++ window.cs
@@ -2,24 +2,32 @@ using System.Diagnostics;
 
 namespace danverter {
 
-
-    public partial class danverter : Form
+    public partial class Danverter : Form
     {
-        private string[] selected_files;
-        
-        public danverter() {
+
+        public Danverter()
+        {
             InitializeComponent();
             this.MinimumSize = new System.Drawing.Size(600, 500);
             this.Size = new System.Drawing.Size(600, 500);
         }
 
-        private void button_browse_files_click(object sender, EventArgs e) {
-
+        private void button_browse_files_click(object sender, EventArgs e)
+        {
             FileExplorer.Instance.open_file_explorer();
+            FileExplorer.Instance.refresh(display_selected_files);
+        }
 
-            foreach (string file in FileExplorer.Instance.get_selected_files()) {
-                Debug.WriteLine(Path.GetFileName(file));
-            }
+        private void button_delete_selected_files_click(object sender, EventArgs e)
+        {
+            FileExplorer.Instance.delete_selected_files(display_selected_files);
+            FileExplorer.Instance.refresh(display_selected_files);
+        }
+
+        private void button_clear_click(object sender, EventArgs e)
+        {
+            FileExplorer.Instance.clear();
+            FileExplorer.Instance.refresh(display_selected_files);
         }
     }
 }
